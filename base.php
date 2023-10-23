0798a659e2ddbbc9a07bda6a28fb3e916f5448c5
Dylan <72610316+dylan-mp4@users.noreply.github.com>
2023-10-23 21:42:58 +0100

    bonzi lookin mf

--- base.php
+++ base.php
@@ -1,32 +1,52 @@
+<?php
+ob_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
+<?php     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo isset($title); ?></title>
-<link
-  rel="stylesheet"
-  href="https://unpkg.com/xp.css">
+    <title><?php echo isset($title); ?></title>
+
+    <link rel="stylesheet" href="https://unpkg.com/xp.css">
+    <link rel="stylesheet" type="text/css" href="clippy.js/src/clippy.css" media="all">
 </head>
+
 <body>
     <nav>
         <div class="navbar">
-            <div class="window" style="width:auto; height:20px">
-                <a href="movieList.php">MyMovieList</a>
+            <div class="title-bar">
+                <div class="title-bar-text"><?php echo isset($title); ?> </div>
+                <div class="title-bar-controls">
+                    <?php
+                    if (isset($_SESSION['username'])) { ?>
+                        <button aria-label="Minimize" onclick="window.location.href='movieList.php'"></button>
+                        <button aria-label="Restore" onclick="window.location.href='index.php'"></button>
+                        <button aria-label="Close" onclick="window.location.href='logout.php'"></button>
+                    <?php } else { ?>
+                        <button aria-label="Minimize" onclick="window.location.href='movieList.php'"></button>
+                        <button aria-label="Restore" onclick="window.location.href='index.php'"></button>
+                        <button aria-label="Help" onclick="window.location.href='logout.php'"></button>
+                    <?php } ?>
+                </div>
             </div>
-            <?php
-            if (isset($_SESSION['username'])){ ?>
-                <div class="nav__auth">
-                    <?php echo $_SESSION['username'];?>
-                    <a href="logout.php">Log out</a>
-                </div>
-            <?php } else { ?>
-                <div class="nav__auth">
-                    <a href="login.php">Login</a>
-                    <a href="register.php">Register</a>
-                </div>
-            <?php } ?>
-            
         </div>
     </nav>
 </body>
+<!-- Add these scripts to  the bottom of the page -->
+<script src="https://unpkg.com/jquery@3.2.1"></script>
+ 
+<script src="clippy.js/build/clippy.js"></script>
+ 
+<script type="text/javascript">
+
+clippy.load('Bonzi', function(agent){
+    // Do anything with the loaded agent
+    agent.show();
+    agent.animate();
+});
+</script> 
