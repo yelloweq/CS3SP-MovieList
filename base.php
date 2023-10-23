491e98591c92b42280f1f2d588e140247d725f63
Dylan <72610316+dylan-mp4@users.noreply.github.com>
2023-10-23 20:17:29 +0100

    fix title

--- base.php
+++ base.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $title; ?></title>
+<title><?php echo isset($title); ?></title>
 <link
   rel="stylesheet"
   href="https://unpkg.com/xp.css">
