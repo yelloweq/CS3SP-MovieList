c05b59c58799134f6c7d21dcd483b74308c51945
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com>
2023-10-30 16:45:07 +0000

    user assistance because this site is awfully designed

--- base.php
+++ base.php
@@ -10,10 +10,11 @@ $title = isset($title) ? $title : "Moviebox";
     header('Content-Type: text/html; charset=utf-8');
 ?>
 <head>
+    <link rel="stylesheet" href="assets/styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php if (isset($title)) { echo $title; } ?></title>
-
+    <link rel="stylesheet" href="assets/styles.css">
     <link rel="stylesheet" href="https://unpkg.com/xp.css">
     <link rel="stylesheet" type="text/css" href="assets/clippy.css" media="all">
 </head>
@@ -31,13 +32,13 @@ $title = isset($title) ? $title : "Moviebox";
                 <div class="title-bar-controls">
                     <?php
                     if (isset($_SESSION['username'])) { ?>
-                        <button aria-label="Minimize" onclick="window.location.href='index.php'"></button>
-                        <button aria-label="Restore" onclick="window.location.href='movieList.php'"></button>
-                        <button aria-label="Close" onclick="window.location.href='logout.php'"></button>
+                        <button aria-label="Minimize" onclick="window.location.href='index'" title="Click to Minimize"></button>
+                        <button aria-label="Restore" onclick="window.location.href='movieList'" title="Go to your MovieList"></button>
+                        <button aria-label="Close" onclick="window.location.href='logout'" title="Logout"></button>
                     <?php } else { ?>
-                        <button aria-label="Minimize" onclick="window.location.href='index.php'"></button>
-                        <button aria-label="Restore" onclick="window.location.href='login.php'"></button>
-                        <button aria-label="Help" onclick="window.location.href='register.php'"></button>
+                        <button aria-label="Minimize" onclick="window.location.href='index'" title="Click to Minimize"></button>
+                        <button aria-label="Restore" onclick="window.location.href='login'" title="Click to Login"></button>
+                        <button aria-label="Help" onclick="window.location.href='register'" title="Click to Register"></button>
                     <?php } ?>
                 </div>
         </div>
