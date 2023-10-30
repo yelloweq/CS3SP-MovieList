bac055e4df14ca2aa93573816610e5fe7deafc87
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com>
2023-10-30 16:43:25 +0000

    need to test this but it should work?

--- db_setup.php
+++ db_setup.php
@@ -1,3 +1,104 @@
 <?php
 
-//add db structure for easier setup
+// Database configuration
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'coursework';
+
+// Connect to MySQL server
+$conn = mysqli_connect($host, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully\n";
+} else {
+    echo "Error creating database: " . mysqli_error($conn) . "\n";
+}
+
+// Select database
+mysqli_select_db($conn, $dbname);
+
+// Table statements
+$sql_statements = array(
+    "CREATE TABLE IF NOT EXISTS users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(30) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(50) UNIQUE,
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )",
+    "CREATE TABLE IF NOT EXISTS movies (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        genre VARCHAR(255) NOT NULL,
+        released_at YEAR(4) NOT NULL
+    )",
+    "CREATE TABLE IF NOT EXISTS movie_reviews (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        movie_id INT(6) UNSIGNED NOT NULL,
+        user_id INT(6) UNSIGNED NOT NULL,
+        review TEXT,
+        rating INT(1) NOT NULL,
+        FOREIGN KEY (movie_id) REFERENCES movies(id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )",
+    "CREATE TABLE IF NOT EXISTS user_movies (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        movie_id INT(6) UNSIGNED NOT NULL,
+        user_id INT(6) UNSIGNED NOT NULL,
+        FOREIGN KEY (movie_id) REFERENCES movies(id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )"
+);
+
+// Loop over SQL statements and execute them
+foreach ($sql_statements as $sql) {
+    if (mysqli_query($conn, $sql)) {
+        echo "Table created successfully\n";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn) . "\n";
+    }
+}
+
+// Insert sample data
+$sql_statements = array(
+    "INSERT INTO users (username, password, email) VALUES
+    ('John', '1234', 'john@example.com'),
+    ('Joe', '1234', 'joe@example.com'),
+    ('James', '1234', 'james@example.com'
+    )",
+    "INSERT INTO movies (title, genre, released_at) VALUES
+    ('The Shawshank Redemption', 'Drama', '1994'),
+    ('The Godfather', 'Crime', '1972'),
+    ('The Dark Knight', 'Action', '2008'
+    )",
+    "INSERT INTO movie_reviews (movie_id, user_id, review) VALUES
+    ('1', '1', 'wow!'),
+    ('1', '2', 'This is a bad movie!'),
+    ('1', '3', 'This is a great movie!'
+    )",
+    "INSERT INTO user_movies (movie_id, user_id) VALUES
+    ('1', '1'),
+    ('1', '2'),
+    ('1', '3')"
+);
+// Loop over SQL statements and execute them
+foreach ($sql_statements as $sql) {
+    if (mysqli_query($conn, $sql)) {
+        echo "Data inserted successfully\n";
+    } else {
+        echo "Error inserting data: " . mysqli_error($conn) . "\n";
+    }
+}
+
+// Close connection
+mysqli_close($conn);
